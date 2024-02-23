467721c3dc8b16af5ba8b62efb3db1a6fc3ab21d
arjun98k <arjunkandekar14@gmail.com>
2024-02-23 12:15:23 +0530

    add file

--- variable.js
+++ variable.js
@@ -1,4 +1,16 @@
-let a  = 5;
-let b = 97;
-let c = a+b;
-console.log("the sum of two number are "+ c );
+// let a  = 5;
+// let b = 97;
+// let c = a+b;
+// console.log("the sum of two number are "+ typeof c + c );
+// let q = true;
+// let g = "amey";
+// let v = undefined;
+// let u = null;
+// console.log(typeof q ,typeof g,typeof v,typeof u);
+let myVar = undefined;
+
+if (myVar) {
+  console.log('myVar has a value'+ "   "+ typeof myVar);
+} else {
+  console.log('myVar is null' +" "+ typeof myVar);
+}
